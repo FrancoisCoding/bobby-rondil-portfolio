1efcc1701e23a41bb887a3bb08ac6f8b64711eed
isaiah francois <francoisisaiaht@yahoo.com>
2020-09-08 23:30:41 -0400

    Added reporter reel

---
+++ contact.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Contact</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+
+<body class="is-preload">
+
+  <!-- Wrapper -->
+
+  <!-- Navigation -->
+  <nav class='navbar'>
+    <div class='headline'>
+      <h1>BOBBY RONDIL
+        <h1 />
+        <h2>Multi-Media JOURNALIST</h2>
+    </div>
+    <div>
+      <div class='links-wrapper'>
+        <ul class="links">
+          <li class='home link'><a href="index.html">Home</a></li>
+          <li class='link'><a href="resume.html">Resume</a></li>
+          <li class='link'><a href='works.html'>Published Works</a></li>
+          <li class='link'><a href='media.html'>Reel</a></li>
+          <li class='link'><a href="gallery.html">Gallery</a></li>
+          <li class='link'><a href="contact.php">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main -->
+  <div class="contactContainer">
+    <div class="wrapper animated bounceInLeft">
+      <div class="company-info">
+        <h1>Contact</h1>
+      </div>
+      <div class="contact">
+        <form>
+          <p>
+            <label>Name</label>
+            <input type="text" name="name" autocomplete="off" required />
+          </p>
+          <p>
+            <label>Company</label>
+            <input type="text" name="company" autocomplete="off" />
+          </p>
+          <p>
+            <label>Email Address</label>
+            <input autocomplete="off" type="email" name="email" required />
+          </p>
+          <p>
+            <label>Phone Number</label>
+            <input autocomplete="off" type="text" name="phone" />
+          </p>
+          <p>
+            <label>Message</label>
+            <textarea rows="5" autocomplete="off" type='text' name="message" required></textarea>
+          </p>
+          <button class="contactBtn">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Footer -->
+  <footer id="footer">
+    <ul class="copyright">
+      <li>&copy; Bobby Rondil</li>
+    </ul>
+  </footer>
+</body>
+
+</html>
